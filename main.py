fcc15f49afc39bd0bc976fdc13897e4212dd57ac
João Vitor <joaovitor.jsr@gmail.com>
2022-03-28 20:29:27 -0300

    adicionados filtros

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telegram import Bot
-from keys import *  
+from keys import *
 import discord
 import re 
 from urllib.request import urlopen, Request
@@ -21,14 +21,35 @@ async def on_message(message):
     response = urlopen(req)
     html = response.read()
     soup = BeautifulSoup(html,'html.parser')
+   
     def manga_caplk(link): 
-                    try:
-                        linkk = itens.find_next('span', class_="font-meta chapter")
-                        link = linkk.find_next(href=True)
-                        return link['href']
-                    except AttributeError:
-                        link = "None"
-                        return link
+        try:
+            linkk = itens.find_next('div', class_="post-title")
+            link = linkk.find_next(href=True)
+            return link['href']
+        except AttributeError:
+            link = "None"
+            return link
+
+    def filtro(messagefilted):
+                if re.findall('<@&'+ '[0-9]+' + '>',messagefilted):
+                    messagefilted = re.sub('<@&'+ '[0-9]+' + '>','' , messagefilted)
+                    return messagefilted
+                if re.findall('<#'+ '[0-9]+' + '>',messagefilted):
+                    messagefilted = re.sub('<#'+ '[0-9]+' + '>' ,'tags',messagefilted)
+                    return messagefilted
+                if re.findall('[**]',messagefilted):
+                    messagefilted = re.sub('[**]','', messagefilted)
+                    return messagefilted
+                else:
+                    return messagefilted 
+
+    def capnumber(messagefilted1):
+        if re.findall('Capítulo [0-9]',messagefilted1):
+            messagefilted1 = re.sub('Capítulo','', messagefilted1)
+            messagefilted1 = [int(temp)for temp in messagefilted1.split() if temp.isdigit()]
+            return messagefilted1
+
     if message.channel.id in DISCORD_CHANNELS:
         if message.attachments:
             attachers = ""
@@ -38,35 +59,35 @@ async def on_message(message):
             for i in range(0,int (1)):
                 tabsitens = soup.find_all('div', class_="row c-tabs-item__content")
                 itens = tabsitens[i]
-
-                
-            messageb = re.sub('<@&'+ '[0-9]+' + '>','', f'{message.content}')
-            messagef2 = re.sub('<#826944584524234754>' ,'#tags',messageb)
-            messagef2 = re.sub('[**]','',messagef2)
-            print(messagef2)
+            linkdefin1 = []
+            for i in range(0,int (len(capnumber(message.content)))):
+                linkdefini = (f'{manga_caplk(any)}'+"/cap-" + f'{capnumber(message.content)[i]}' )
+                linkdefin1.extend([linkdefini])
+            
+            lin_new = [str(a) for a in linkdefin1]
             try:
-                tg.sendMessage(TELEGRAM_CHAT_ID, messagef2 + "\n"  + manga_caplk(all) )
-                print("Enviado 1")
+                  tg.sendMessage(TELEGRAM_CHAT_ID, filtro(message.content) + "\n" + "\n". join(lin_new)  )
+                  print("Enviado If: " + filtro(message.content) + "\n" + "\n". join(lin_new) )
             except:
-                print("Não enviado 1")
+                  print("Não enviado if")
+
         else:
             for i in range(0,int (1)):
                 tabsitens = soup.find_all('div', class_="row c-tabs-item__content")
                 itens = tabsitens[i]
-
-                
-            messageb = re.sub('<@&'+ '[0-9]+' + '>','', f'{message.content}')
-            messagef2 = re.sub('<#826944584524234754>' ,'#tags',messageb)
-            messagef2 = re.sub('[**]','',messagef2)
-            print(messagef2)
+            linkdefin1 = []
             try:
-                tg.sendMessage(TELEGRAM_CHAT_ID, messagef2 + "\n"  + manga_caplk(all) )
-                print("Enviado 2")
+                for i in range(0,int (len(capnumber(message.content)))):
+                    linkdefini = (f'{manga_caplk(any)}'+"/cap-" + f'{capnumber(message.content)[i]}' )
+                    linkdefin1.extend([linkdefini])
             except:
-                print("Não enviado 2")
-        
-    
-
+                pass
+            lin_new = [str(a) for a in linkdefin1]
+            try:
+                  tg.sendMessage(TELEGRAM_CHAT_ID, filtro(message.content) + "\n" + "\n". join(lin_new)  )
+                  print("Enviado Else: " + filtro(message.content) + "\n" + "\n". join(lin_new) )
+            except:
+                  print("Não enviado else")
 
 def main():
     print("Running, waiting for messages...")
