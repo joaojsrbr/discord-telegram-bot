8220ab2475fb5d40f5666f267c9c121fcf8f0ce2
João Vitor <joaovitor.jsr@gmail.com>
2022-03-30 01:48:36 -0300

    new

--- meth.py
+++ meth.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 
 
-url = "https://neoxscans.net"
+url = "https://neoxscans.net/manga?m_orderby=latest"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"}        
 req = Request(url,headers=headers)
 response = urlopen(req)
@@ -13,16 +13,12 @@ soup = BeautifulSoup(html,'html.parser')
 
 
 def manga_itens(itens):
-        tabsitens1 = soup.find('div', class_="row row-eq-height")
-        tabsitens2 = tabsitens1.find('div', class_="col-6 col-md-2 badge-pos-2")
-        tabsitens3 = tabsitens2.find('div', class_="item-thumb c-image-hover")
-        itens = tabsitens3
+        itens = soup.find('div', class_="page-content-listing item-default").find('div', class_="page-listing-item").find('div', class_="row row-eq-height").find('div', class_="col-12 col-md-6 badge-pos-2").find('div', class_="page-item-detail manga").find('h3', class_="h5").find('a')
         return itens
 
 def manga_caplk(link): 
         try:
-            linkk = manga_itens(all).find('a')
-            link = linkk['href']
+            link = manga_itens(all)['href']
             return link
         except AttributeError:
             link = "None"
